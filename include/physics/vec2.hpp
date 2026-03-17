040d6549bcfc73a07d06043e03fdefa3b8172875
WuXing-ovo <wuxingovo@icloud.com>
2026-03-17 14:48:45 +0800

    feat: add dot product for vec2

--- include/physics/vec2.hpp
+++ include/physics/vec2.hpp
@@ -6,7 +6,7 @@ struct vec2
     double y;
 
     // Constructors
-    vec2(): x(0), y(0) {}
+    vec2() : x(0), y(0) {}
     vec2(double x_in, double y_in) : x(x_in), y(y_in) {}
     // Operator overloads
     vec2 operator+(const vec2 &vec2_in) const
@@ -36,5 +36,9 @@ struct vec2
         x -= vec2_in.x;
         y -= vec2_in.y;
         return *this;
+    }
+    double dot(const vec2 &other) const
+    {
+        return x * other.x + y * other.y;
     }
 };
