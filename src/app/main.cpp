c8b61853c159aa50938465e9ae34f2035b16abce
WuXing-ovo <wuxingovo@icloud.com>
2026-06-26 21:23:30 +0800

    feat: add Raylib-based visualization (draw balls, show FPS)

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,20 +1,53 @@
 #include <iostream>
+#include <span>
 #include "physics/ball.hpp"
 #include "physics/world.hpp"
+#include "raylib.h"
 
 int main()
 {
     World world;
-    world.set_size(800, 600);
+    world.set_size(1920, 1080);
     world.set_gravity(0, -9.8);
 
     // Add two balls with random properties
-    world.add_random_Balls(4, vec2{-100, -100}, vec2{100, 100}, 1, 5, 5, 10);
+    world.add_random_Balls(100, vec2{-100, -100}, vec2{100, 100}, 1, 5, 5, 10);
 
-    // Simulation
-    for (int frame = 0; frame < 1000; ++frame)
+    // Visualization
+    // Set screen size
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+
+    // Initialize the screen
+    InitWindow(screenWidth, screenHeight, "RigidBody2D");
+
+    // Set fps
+    int Fps = 60;
+    SetTargetFPS(Fps);
+
+    // Main loop
+    while(!WindowShouldClose())
     {
-        world.print_state();
         world.simulate(1);
+        world.print_state();
+        // Get Balls
+        std::span<const Ball> all_balls = world.get_Balls(0, 100);
+
+        BeginDrawing;
+        ClearBackground(WHITE);
+        // Add balls to the screen
+        for(const auto& ball : all_balls){
+            auto position = ball.get_position();
+            auto radius = ball.get_size();
+
+            DrawCircle(position.x, position.y, radius, BLUE);
+        }
+
+        // Draw frame informations
+        DrawText(TextFormat("FPS: %i", GetFPS()), 10, 10, 20, DARKGRAY);
+        DrawText(TextFormat("Frame time: %02.02f ms", GetFrameTime()), 10, 30, 20, DARKGRAY);
+
+        EndDrawing();
     }
+    CloseWindow();
 }
